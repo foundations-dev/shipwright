90dfda322de8329a5499159a0edbbc72e8ea990c
earlman <earl.mangulabnan@gmail.com>
2019-06-16 14:55:44 -0500

    add directory-structure.md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,6 +10,7 @@ module.exports = {
 
                     'getting-started',
                     'principles',
+                    'directory-structure',
                 ]
             },
             {
